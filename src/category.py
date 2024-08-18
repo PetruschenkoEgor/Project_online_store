1c6bdcc43bcfed81ff4dcd4330ee69f192efce36
Egor Petruschenko <jaguar.94@mail.ru>
2024-08-18 20:54:33 +0600

    add class Category and main

---
+++ src/category.py
@@ -0,0 +1,22 @@
+from src.product import Product
+
+
+class Category:
+    """ Класс для представления категорий продуктов """
+    name: str
+    description: str
+    products: list
+    category_count = 0
+    product_count = 0
+
+    def __init__(self, name, description, products):
+        """ Метод для инициализации экземпляра класса """
+        self.name = name
+        self.description = description
+        self.products = products
+
+        # Количество категорий
+        Category.category_count += len(products)
+        # Количество товаров
+        for product in products:
+            Category.product_count += product.quantity
