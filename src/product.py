3a8b161b03af342d66bed58e8b05dc93c0ebcb2e
Egor Petruschenko <jaguar.94@mail.ru>
2024-08-22 16:49:54 +0600

    add price

--- src/product.py
+++ src/product.py
@@ -10,14 +10,27 @@ class Product:
         """Метод для инициализации экземпляра класса"""
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
 
     @classmethod
-    def new_product(cls, new_product_dict):
+    def new_product(cls, new_product_dict, ):
         """ Создает объекты класса Product """
         name = new_product_dict.get("name")
         description = new_product_dict.get("description")
         price = new_product_dict.get("price")
         quantity = new_product_dict.get("quantity")
         return cls(name, description, price, quantity)
+
+    @property
+    def price(self):
+        """ Показывает цену """
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        """ Изменяет цену """
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = new_price
