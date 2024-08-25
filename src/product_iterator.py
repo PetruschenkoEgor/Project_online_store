381cad57a1edc9ad0e387311afda0eda47999278
Egor Petruschenko <jaguar.94@mail.ru>
2024-08-25 16:04:14 +0600

    add iteratot

---
+++ src/product_iterator.py
@@ -0,0 +1,18 @@
+class ProductIterator:
+    def __init__(self, product_obj):
+        self.category = product_obj
+        self.index = 0
+
+    def __iter__(self):
+        """ Возвращает итератор """
+        self.index = 0
+        return self
+
+    def __next__(self):
+        """ Возвращает следующий элемент последовательности """
+        if self.index < len(self.category.products_list):
+            product = self.category.products_list[self.index]
+            self.index += 1
+            return product
+        else:
+            raise StopIteration
