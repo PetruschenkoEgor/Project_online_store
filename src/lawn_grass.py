a522690087d2110337a2d0f824655d2888d18999
Egor Petruschenko <jaguar.94@mail.ru>
2024-08-27 11:49:05 +0600

    add LawnGrass __init__

--- src/lawn_grass.py
+++ src/lawn_grass.py
@@ -1,3 +1,14 @@
-class LawnGrass:
+from src.product import Product
+
+
+class LawnGrass(Product):
     """ Категория товаров с травой газанной """
-    pass
+    def __init__(self, name, description, price, quantity, country, germination_period, color):
+        """ Конструктор класса LawnGrass """
+        super().__init__(name, description, price, quantity)
+        # Страна-производитель
+        self.country = country
+        # Срок прорастания
+        self.germination_period = germination_period
+        # Цвет
+        self.color = color
